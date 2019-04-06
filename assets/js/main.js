091efce97161f7ac960010ff02d0cd9391b5cd04
LeoHolman <lah2719@g.rit.edu>
2019-04-06 16:00:48 -0400

    only one active picture at a time
    text returns to previous more easily

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,50 @@ const trudge = document.getElementById("trudge");
 const nightmare = document.getElementById("nightmare");
 
 var text;
+var clicked = false;
+var lastClicked;
+
 function wordToPicture(divID){
-    let div = document.getElementById(divID);
-    text = div.innerHTML;
-    div.innerHTML = '';
-    let newPictureNode = document.createElement("IMG");
-    newPictureNode.src = `assets/images/${divID}/${divID}1.jpg`;
-    div.appendChild(newPictureNode);
+    if(lastClicked !== divID){
+        if(lastClicked){
+            pictureToWord(lastClicked);
+        }
+        let div = document.getElementById(divID);
+        text = div.innerHTML;
+        div.innerHTML = '';
+        let newPictureNode = document.createElement("IMG");
+        newPictureNode.src = `assets/images/${divID}/${divID}1.jpg`;
+        div.appendChild(newPictureNode);
+        clicked = true;
+        
+        lastClicked = divID;
+
+    }
 }
 
 function pictureToWord(divID){
-    let div = document.getElementById(divID);
-    div.innerHTML = text;
+    if(clicked){
+        let div = document.getElementById(divID);
+        while(div.firstChild){
+            div.removeChild(div.firstChild);
+        }
+        div.innerHTML = text;
+        clicked = false;
+    }
 }
 
-path.addEventListener("mouseover", () => {
-    wordToPicture("path");
+path.addEventListener("click", () => {
+    wordToPicture("path");  
 });
 
-path.addEventListener("mouseout", () => {
-    pictureToWord("path");
-})
+darkness.addEventListener("click", () => {
+    wordToPicture("darkness");  
+});
+
+trudge.addEventListener("click", () => {
+    wordToPicture("trudge");  
+});
+
+nightmare.addEventListener("click", () => {
+    wordToPicture("nightmare");  
+});
